e31622ae2734ae68ca34b9f957295f589efd7782
nerusskyhigh <guglielmo.grillo@unitn.it>
2026-04-05 20:39:25 +0200

    migrate to astro

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Guglielmo Grillo | NerusSkyhigh";
+export const SITE_DESCRIPTION =
+	"PhD student in Physics at the University of Trento with a strong passion for computer science, computational physics, and scientific communication.";
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
